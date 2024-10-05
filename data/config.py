999210f8c8f90b26c20b37e30c526e6018d0ff5d
DefiWimar <wimarxt@gmail.com>
2024-10-05 11:58:15 +0200

    Add files via upload

---
+++ data/config.py
@@ -0,0 +1,46 @@
+# API ID, hash
+API_ID = 11111111
+API_HASH = 'Your API hash'
+
+USE_TG_BOT = False  # True if you want to use Telegram bot, else False
+BOT_TOKEN = 'YOUR_BOT_TOKEN'  # API TOKEN obtained from @BotFather
+CHAT_ID = 'YOUR_TELEGRAM_ID'  # Your Telegram ID
+
+# Delay between connecting to accounts
+ACC_DELAY = [5, 15]
+
+# Type of proxy
+PROXY_TYPE = "socks5"  # http/socks5
+
+# Folder with sessions (do not change)
+WORKDIR = "sessions/"
+
+# Use proxy
+USE_PROXY = True  # True/False
+
+# How many points from the game
+POINTS = [100, 140]  # [min, max]
+
+# Spend diamonds
+SPEND_DIAMONDS = True  # True/False
+
+# Do tasks
+DO_TASKS = True
+
+# Sleep between games
+SLEEP_GAME_TIME = [30, 50]  # [min, max]
+
+# Mini delays
+MINI_SLEEP = [3, 7]  # [min, max]
+
+# Additional delay after 8 hours
+SLEEP_8HOURS = [60*60, 120*60]  # [min, max] seconds
+
+hello = '''                                       
+ ____        __ ___        ___                      
+|  _ \  ___ / _(_) \      / (_)_ __ ___   __ _ _ __ 
+| | | |/ _ \ |_| |\ \ /\ / /| | '_ ` _ \ / _` | '__|
+| |_| |  __/  _| | \ V  V / | | | | | | | (_| | |   
+|____/ \___|_| |_|  \_/\_/  |_|_| |_| |_|\__,_|_|
+
+'''
